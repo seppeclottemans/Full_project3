988d397aeae26a7d32c80fda5c8206dcf3d154db
Seppe Clottemans <>
2020-01-14 09:53:37 +0100

    get request

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,19 @@
 const express = require('./node_modules/express');
 const app = express();
-var bodyParser = require("./node_modules/body-parser");
+const bodyParser = require("./node_modules/body-parser");
 const axios = require('./node_modules/axios').default;
 const sha256 = require("./node_modules/js-sha256");
 const port = 3000;
 
-var key = "2dadbed20e3367139efb39ccc110d335b1497f36f3bbbebc822ff90b9d637b85";
+let key = "2dadbed20e3367139efb39ccc110d335b1497f36f3bbbebc822ff90b9d637b85";
+let user = "admin";
 
-
-var query = "user=admin" + "&function=get_user_collections";
-var signedRequestString = sha256(key + query);
-console.log(signedRequestString);
-
-axios.get( `http://minikmska.trial.resourcespace.com/api/?user=admin&function=get_user_collections&sign=${signedRequestString}`
+// get all user collections
+get_user_collections();
+function get_user_collections(){
+  let query = `user=${user}&function=get_user_collections`;
+  let signedRequestString = sha256(key + query);
+  axios.get( `http://minikmska.trial.resourcespace.com/api/?${query}&sign=${signedRequestString}`
 ).then(function (response) {
     console.log(response);
   })
@@ -20,3 +21,47 @@ axios.get( `http://minikmska.trial.resourcespace.com/api/?user=admin&function=ge
     console.log("dit is een ERROR !!!!")
     console.log(error);
   });
+};
+
+// get all public collections
+function get_collections(){
+  let query = `user=${user}&function=search_public_collections`;
+  let signedRequestString = sha256(key + query);
+  axios.get( `http://minikmska.trial.resourcespace.com/api/?${query}&sign=${signedRequestString}`
+).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log("dit is een ERROR !!!!")
+    console.log(error);
+  });
+};
+
+// get all data of a specific painting
+function get_painting(resourceID){
+  let query = `user=${user}&function=get_resource_data&param1=${resourceID}`;
+  let signedRequestString = sha256(key + query);
+  axios.get( `http://minikmska.trial.resourcespace.com/api/?${query}&sign=${signedRequestString}`
+).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log("dit is een ERROR !!!!")
+    console.log(error);
+  });
+};
+
+// get painting image
+function get_image(resourceID){
+  let query = `user=${user}&function=get_resource_path&param1=${resourceID}&param2=false`;
+  let signedRequestString = sha256(key + query);
+  axios.get( `http://minikmska.trial.resourcespace.com/api/?${query}&sign=${signedRequestString}`
+).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log("dit is een ERROR !!!!")
+    console.log(error);
+  });
+};
+
