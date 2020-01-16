8747b69ae8d36b99a958e41ba1f3b9c6b8422be2
Seppe Clottemans <>
2020-01-16 13:39:35 +0100

    modules

---
+++ Backend/painting.js
@@ -0,0 +1,13 @@
+class Painting {
+    constructor(id, title, artist, year, image, info, tags) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.image = image;
+        this.info = info;
+        this.tags = tags;
+    }
+}
+
+export default Painting;
