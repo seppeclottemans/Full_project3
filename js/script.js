d33d9eb5a985161dfe9fb93ec656fbde68fdbbd1
Elise Boes <elise.boes@student.ehb.be>
2020-01-21 10:44:23 +0100

    Star rating fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 $(function () {
     let appendRoutes = (allRoutes) => {
         function getURLParameter(name) {
@@ -7,11 +5,11 @@ $(function () {
         }
 
         let getSelectedRoute = (allRoutes) => {
-            let selectedRoute = getURLParameter('id');
+            let selectedRoute = getURLParameter('name');
+            console.log(selectedRoute);
             for (let route of allRoutes) {
-                if (route._id == selectedRoute) {
+                if (route.name == selectedRoute) {
                     $('#route-inf').append(`
-                    <h1>${route.name}</h1>
                     <img src="${route.images[0]}" alt="">
                     <p>${route.info}</p>
                     <div>
@@ -20,46 +18,14 @@ $(function () {
                         </div>
                         <div class="route-inf-extra">
                             <p>Duration:</p>
-                            <p>2:45h</p>
+                            <p class="redorange">2:45h</p>
                         </div>
                     </div>
-                        <button class="btn">
-                        <a href="routeinstructions.html">
-                            <p>Select route</p>
-                        </a>
-                    </button>`)
+                    <button class="btn"><a href="routeinstructions.html">Select route</a></button>
+`)
                 }
             }
-
-        //     let selectedRoute = getURLParameter('id');
-        //     //$.getJSON(`http://localhost:3000/getRouteMongo/${selectedRoute}`, function (route) {
-        //     $.getJSON(`/routes.json`, function (routes) {
-        //         for (let route of routes) {
-        //             if (selectedRoute == route._id) {
-        //                 $('#loading_screen').hide();
-        //                 //appendRoutes(d);
-        //                 console.log(route);
-        //                 $('#route-inf').append(`
-        // <img src="${route.images[0]}" alt="">
-        // <p>${route.info}</p>
-        // <div>
-        //     <div class="route-inf-extra">
-        //         <p>Rating:</p><div class="starrr"></div>
-        //     </div>
-        //     <div class="route-inf-extra">
-        //         <p>Duration:</p>
-        //         <p>2:45h</p>
-        //     </div>
-        // </div>
-        //     <div class="btn">
-        // <a href="routeinstructions.html"><p>Select route</p></a>
-        //     </div>`)
-        //             }
-        //         }
-        //     }).fail(function (e1, e2) {
-        //         console.log(e1);
-        //         console.log(e2);
-        //     });
+            $('.starrr').starrr();
         }
 
         getSelectedRoute(allRoutes);
@@ -68,7 +34,7 @@ $(function () {
             $('.carousel').append(`<figure>
             <img src="${route.images[0]}" alt="">
        <figcaption>
-            <h2 id="${route._id}">${route.name}</h2>
+            <h2 id="${route.name}">${route.name}</h2>
             <p>${route.info}</p>
             <button class="btn readmore"><a href="routeinf.html">Read more >></a></button>
         </figcaption>
@@ -77,15 +43,15 @@ $(function () {
 
         $('.carousel figure img').click(function () {
             selectedRoute = $($($(this).siblings()[0]).children()[0]).attr('id');
-            window.location.href = `routeinf.html?id=${selectedRoute}`
+            window.location.href = `routeinf.html?name=${selectedRoute}`
         });
-        $('.carousel .readmore').click(function () {
+        $('.carousel .readmore').click(function(){
             selectedRoute = $($($(this).siblings()[0]).children()[0]).attr('id');
-            window.location.href = `routeinf.html?id=${selectedRoute}`
+            window.location.href = `routeinf.html?name=${selectedRoute}`
         });
     }
 
-    $.getJSON('http://localhost:3000/getAllRoutesMongo', function (d) {
+    $.getJSON('js/routes.json', function (d) {
         $('#loading_screen').hide();
         appendRoutes(d);
     }).fail(function (e1, e2) {
@@ -95,31 +61,31 @@ $(function () {
 });
 
 
-// getAllRoutesMongo();
-// // return all routes as array of object
-// function getAllRoutesMongo() {
-//     $.ajax({
-//         url: "http://localhost:3000/getAllRoutesMongo",
-//         method: 'GET'
-//     }).done(function (data) {
-//         console.log(data);
-//     }).fail(function (err1, err2) {
-//         console.log('Fail');
-//         console.log(err1);
-//         console.log(err2);
-//     });
-// }
+getAllRoutesMongo();
+// return all routes as array of object
+function getAllRoutesMongo(){
+    $.ajax({
+        url: "http://localhost:3000/getAllRoutesMongo",
+        method: 'GET'
+    }).done(function (data) {
+        console.log(data);
+    }).fail(function (err1, err2) {
+        console.log('Fail');
+        console.log(err1);
+        console.log(err2);
+    });
+}
 
-// function getRouteMongo() {
-//     // let routeId;
-//     $.ajax({
-//         url: `http://localhost:3000/getRouteMongo/${routeId}`,
-//         method: 'GET'
-//     }).done(function (data) {
-//         console.log(data);
-//     }).fail(function (err1, err2) {
-//         console.log('Fail');
-//         console.log(err1);
-//         console.log(err2);
-//     });
-// }
+function getRouteMongo(){
+    // let routeId;
+    $.ajax({
+        url: `http://localhost:3000/getRouteMongo/${routeId}`,
+        method: 'GET'
+    }).done(function (data) {
+        console.log(data);
+    }).fail(function (err1, err2) {
+        console.log('Fail');
+        console.log(err1);
+        console.log(err2);
+    });
+}
