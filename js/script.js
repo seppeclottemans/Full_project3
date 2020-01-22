aa4e21f96a2cfaa82e3b75fd3df3bf8413fc6b20
Elise Boes <elise.boes@student.ehb.be>
2020-01-22 09:39:40 +0100

    Loading screen & ajax call fixes

--- js/script.js
+++ js/script.js
@@ -1,65 +1,36 @@
 $(function () {
-    if($('.carousel').length){
-    $.getJSON('http://localhost:3000/getAllRoutesMongo', function (d) {
-        $('#loading_screen').hide();
-        appendRoutes(d);
-    }).fail(function (e1, e2) {
-        console.log(e1);
-        console.log(e2);
-    });
+    let displaySelectedRoute = (route) => {
+        route = JSON.parse(route);
+        $('#route-inf').append(`
+            <h1>${route.name}</h1>
+                            <img src="${route.images[0]}" alt="">
+                            <p>${route.info}</p>
+                            <div>
+                                <div class="route-inf-extra">
+                                    <p>Rating:</p><div class="existingroute-rating starrr"></div>
+                                </div>
+                                <div class="route-inf-extra">
+                                    <p>Duration:</p>
+                                    <p class="redorange">2:45h</p>
+                                </div>
+                            </div>
+                            <button class="btn"><a href="routeinstructions.html">Select route</a></button>
+        `)
+        route.images.forEach(function (routeImg) {
+            $('#route-gallery').append(`<img src="${routeImg}">`)
+        });
+        $('.selections-rating').starrr().on('starrr:change', function (e, value) {
+            console.log(value)
+        })
+        $('.generated-rating').starrr().on('starrr:change', function (e, value) {
+            console.log(value)
+        })
+        $('.existingroute-rating').starrr({
+            rating: 4
+        })
     }
+
     let appendRoutes = (allRoutes) => {
-
-        let getSelectedRoute = (allRoutes) => {
-            let routeId = localStorage.getItem('selectedRoute');
-            $.ajax({
-                url: `http://localhost:3000/getRouteMongo/${routeId}`,
-                method: 'GET'
-            }).done(function (data) {
-                console.log(data);
-            }).fail(function (err1, err2) {
-                console.log('Fail');
-                console.log(err1);
-                console.log(err2);
-            });
-            // $.getJSON(`http://localhost:3000/getRouteMongo/${routeId}`, function (d) {
-            //     $('#loading_screen').hide();
-            //     console.log(d);
-            // }).fail(function (e1, e2) {
-            //     console.log(e1);
-            //     console.log(e2);
-            // });
-            //             for (let route of allRoutes) {
-            //                 if (route.name == selectedRoute) {
-            //                     $('#route-inf').append(`
-            //                     <img src="${route.images[0]}" alt="">
-            //                     <p>${route.info}</p>
-            //                     <div>
-            //                         <div class="route-inf-extra">
-            //                             <p>Rating:</p><div class="existingroute-rating starrr"></div>
-            //                         </div>
-            //                         <div class="route-inf-extra">
-            //                             <p>Duration:</p>
-            //                             <p class="redorange">2:45h</p>
-            //                         </div>
-            //                     </div>
-            //                     <button class="btn"><a href="routeinstructions.html">Select route</a></button>
-            // `)
-            //                 }
-            //             }
-            $('.selections-rating').starrr().on('starrr:change', function (e, value) {
-                console.log(value)
-            })
-            $('.generated-rating').starrr().on('starrr:change', function (e, value) {
-                console.log(value)
-            })
-            $('.existingroute-rating').starrr({
-                rating: 4
-            })
-        }
-
-        getSelectedRoute(allRoutes);
-
         for (let route of allRoutes) {
             $('.carousel').append(`<figure>
            <a href="routeinf.html"><img src="${route.images[0]}" alt="" class="${route._id}"></a>
@@ -80,34 +51,30 @@ $(function () {
             localStorage.setItem('selectedRoute', selectedRoute);
         });
     }
+
+    if ($('.carousel').length) {
+        $.getJSON('http://localhost:3000/getAllRoutesMongo', function (d) {
+            $('#loading_screen').hide();
+            appendRoutes(d);
+        }).fail(function (e1, e2) {
+            console.log(e1);
+            console.log(e2);
+        });
+    }
+
+    if ($('#route-inf').length) {
+        $('#loading_screen').show();
+        let routeId = localStorage.getItem('selectedRoute');
+        $.ajax({
+            url: `http://localhost:3000/getRouteMongo/${routeId}`,
+            method: 'GET'
+        }).done(function (data) {
+            $('#loading_screen').hide();
+            displaySelectedRoute(data);
+        }).fail(function (err1, err2) {
+            console.log('Fail');
+            console.log(err1);
+            console.log(err2);
+        });
+    }
 });
-
-
-// getAllRoutesMongo();
-// // return all routes as array of object
-// function getAllRoutesMongo(){
-//     $.ajax({
-//         url: "http://localhost:3000/getAllRoutesMongo",
-//         method: 'GET'
-//     }).done(function (data) {
-//         console.log(data);
-//     }).fail(function (err1, err2) {
-//         console.log('Fail');
-//         console.log(err1);
-//         console.log(err2);
-//     });
-// }
-
-// function getRouteMongo(){
-//     // let routeId;
-//     $.ajax({
-//         url: `http://localhost:3000/getRouteMongo/${routeId}`,
-//         method: 'GET'
-//     }).done(function (data) {
-//         console.log(data);
-//     }).fail(function (err1, err2) {
-//         console.log('Fail');
-//         console.log(err1);
-//         console.log(err2);
-//     });
-// }
